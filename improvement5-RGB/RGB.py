3b5d248d28c31f28266444c565ed103af3d8ede9
yysmwt <liuyw05@163.com>
2025-01-13 20:37:23 +0800

    improvement5 RGB

---
+++ improvement5-RGB/RGB.py
@@ -0,0 +1,206 @@
+import os
+os.environ['CURL_CA_BUNDLE'] = ''
+os.environ["http_proxy"] = "http://127.0.0.1:7890"
+os.environ["https_proxy"] = "http://127.0.0.1:7890"
+
+# 添加source文件夹所在路径，确保其中的文件被正确导入
+import sys
+sys.path.append('/root/autodl-tmp/cv-final-project')
+
+import numpy as np
+import rp
+import torch
+import torch.nn as nn
+import source.stable_diffusion as sd
+from easydict import EasyDict
+from source.learnable_textures import LearnableImageFourier
+from source.stable_diffusion_labels import NegativeLabel
+from itertools import chain
+import time
+#ONLY GOOD PROMPTS HERE
+example_prompts = rp.load_yaml_file('/root/autodl-tmp/cv-final-project/source/example_prompts.yaml')
+print('Available example prompts:', ', '.join(example_prompts))
+
+#These prompts are all strings - you can replace them with whatever you want! By default it lets you choose from example prompts
+#Here are some on the site! It does take some thought to come up with good ideas; the upside-down should look vaguely like the right-side up to work nicely
+prompt_a, prompt_b = rp.gather(example_prompts, 'victorial_dress victorial_dress'.split())
+prompt_a, prompt_b = rp.gather(example_prompts, 'pencil_giraffe_head pencil_penguin'.split())
+# prompt_a, prompt_b = rp.gather(example_prompts, 'sailing_ship sailing_ship'.split())
+prompt_a, prompt_b,prompt_c,prompt_d = rp.gather(example_prompts, 'mushroom lipstick darth_vader miku'.split())
+
+negative_prompt = ''
+
+print()
+print('Negative prompt:',repr(negative_prompt))
+print()
+print('Chosen prompts:')
+print('    prompt_a =', repr(prompt_a)) #This will be right-side up
+print('    prompt_b =', repr(prompt_b)) #This will be upside-down
+
+### hugging face login
+# 请在终端输入 huggingface-cli login
+# 会产生登录交互，但你不用真的登录，你只需要按照指示去hf官网注册一个账号，然后创建一个新的token，并将token赋值给变量
+# 我的这段代码可以直接登录hf，省去不少工作。
+from huggingface_hub import login
+import subprocess
+
+result = subprocess.run('bash -c "source /etc/network_turbo && env | grep proxy"', shell=True, capture_output=True, text=True)
+output = result.stdout
+for line in output.splitlines():
+    if '=' in line:
+        var, value = line.split('=', 1)
+        os.environ[var] = value
+
+token = "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"
+login(token)
+print("log in successfully.")
+
+if 's' not in dir():
+    model_name="CompVis/stable-diffusion-v1-4"
+    gpu='cuda:0'
+    s=sd.StableDiffusion(gpu,model_name)
+device=s.device
+label_a = NegativeLabel(prompt_a,negative_prompt)
+label_b = NegativeLabel(prompt_b,negative_prompt)
+label_c = NegativeLabel(prompt_c,negative_prompt)
+label_d = NegativeLabel(prompt_d,negative_prompt)
+#Image Parametrization and Initialization (this section takes vram)
+
+#Select Learnable Image Size (this has big VRAM implications!):
+#Note: We use implicit neural representations for better image quality
+#They're previously used in our paper "TRITON: Neural Neural Textures make Sim2Real Consistent" (see tritonpaper.github.io)
+# ... and that representation is based on Fourier Feature Networks (see bmild.github.io/fourfeat)
+learnable_image_maker = lambda: LearnableImageFourier(height=256, width=256, hidden_dim=256, num_features=128).to(s.device); SIZE=256
+# learnable_image_maker = lambda: LearnableImageFourier(height=512,width=512,num_features=256,hidden_dim=256,scale=20).to(s.device);SIZE=512
+
+image=learnable_image_maker()
+#learnable_image_a=lambda: image() #Right-side up
+#learnable_image_b=lambda: image().rot90(k=2,dims=[1,2]) #Upside-down
+#保留红色通道
+def retain_red_channel(image):
+    img_tensor = image()
+    red_channel = img_tensor[0:1, :, :]
+    zeros = torch.zeros_like(red_channel)
+    red_image = torch.cat([red_channel, zeros, zeros], dim=0)
+    return red_image
+
+# 保留绿色通道
+def retain_green_channel(image):
+    img_tensor = image()
+    green_channel = img_tensor[1:2, :, :]
+    zeros = torch.zeros_like(green_channel)
+    green_image = torch.cat([zeros, green_channel, zeros], dim=0)
+    return green_image
+# 保留蓝色通道
+def retain_blue_channel(image):
+    img_tensor = image()
+    blue_channel = img_tensor[2:3, :, :]
+    zeros = torch.zeros_like(blue_channel)
+    blue_image = torch.cat([zeros, zeros, blue_channel], dim=0)
+    return blue_image
+
+learnable_image_a = lambda: image()
+learnable_image_b = lambda: retain_red_channel(image)
+learnable_image_c = lambda: retain_blue_channel(image)
+learnable_image_d = lambda: retain_green_channel(image)
+'''分割线'''
+optim=torch.optim.SGD(image.parameters(),lr=1e-4)
+
+labels=[label_a,label_b,label_c,label_d]
+learnable_images=[learnable_image_a,learnable_image_b,learnable_image_c,learnable_image_d]
+
+#The weight coefficients for each prompt. For example, if we have [0,1], then only the upside-down mode will be optimized
+weights=[1,1,1,1]
+
+weights=rp.as_numpy_array(weights)
+weights=weights/weights.sum()
+weights=weights*len(weights)
+#For saving a timelapse
+ims=[]
+def get_display_image():
+    return rp.tiled_images(
+        [
+            rp.as_numpy_image(learnable_image_a()),
+            rp.as_numpy_image(learnable_image_b()),
+            rp.as_numpy_image(learnable_image_c()),
+            rp.as_numpy_image(learnable_image_d()),
+            rp.as_numpy_image(image())
+        ],
+        length=len(learnable_images),
+        border_thickness=0,
+    )
+NUM_ITER=10000
+
+#Set the minimum and maximum noise timesteps for the dream loss (aka score distillation loss)
+s.max_step=MAX_STEP=990
+s.min_step=MIN_STEP=10 
+
+display_eta=rp.eta(NUM_ITER, title='Status')
+
+DISPLAY_INTERVAL = 200
+
+print('Every %i iterations we display an image in the form [image_a, image_b], where'%DISPLAY_INTERVAL)
+print('    image_a = (the right-side up image)')
+print('    image_b = (image_a, but upside down)')
+print()
+print('Interrupt the kernel at any time to return the currently displayed image')
+print('You can run this cell again to resume training later on')
+print()
+print('Please expect this to take quite a while to get good images (especially on the slower Colab GPU\'s)! The longer you wait the better they\'ll be')
+
+try:
+    for iter_num in range(NUM_ITER):
+        display_eta(iter_num) #Print the remaining time
+
+        preds=[]
+        for label,learnable_image,weight in rp.random_batch(list(zip(labels,learnable_images,weights)), batch_size=1):
+            pred=s.train_step(
+                label.embedding,
+                learnable_image()[None],
+
+                #PRESETS (uncomment one):
+                noise_coef=.1*weight,guidance_scale=60,#10
+                # noise_coef=0,image_coef=-.01,guidance_scale=50,
+                # noise_coef=0,image_coef=-.005,guidance_scale=50,
+                # noise_coef=.1,image_coef=-.010,guidance_scale=50,
+                # noise_coef=.1,image_coef=-.005,guidance_scale=50,
+                # noise_coef=.1*weight, image_coef=-.005*weight, guidance_scale=50,
+            )
+            preds+=list(pred)
+
+        with torch.no_grad():
+            if iter_num and not iter_num%(DISPLAY_INTERVAL*50):
+                #Wipe the slate every 50 displays so they don't get cut off
+                from IPython.display import clear_output
+                clear_output()
+
+            if not iter_num%DISPLAY_INTERVAL:
+                im = get_display_image()
+                ims.append(im)
+                rp.display_image(im)
+
+        optim.step()
+        optim.zero_grad()
+except KeyboardInterrupt:
+    print()
+    print('Interrupted early at iteration %i'%iter_num)
+    im = get_display_image()
+    ims.append(im)
+    rp.display_image(im)
+print('Right-side up image:')
+rp.display_image(rp.as_numpy_image(learnable_image_a()))
+
+print('Upside-down image:')
+rp.display_image(rp.as_numpy_image(learnable_image_b()))
+def save_run(name):
+    folder="/root/autodl-tmp/cv-final-project/output-RGB: %s"%name
+    if rp.path_exists(folder):
+        folder+='_%i'%time.time()
+    rp.make_directory(folder)
+    ims_names=['ims_%04i.png'%i for i in range(len(ims))]
+    with rp.SetCurrentDirectoryTemporarily(folder):
+        rp.save_images(ims,ims_names,show_progress=True)
+    print()
+    print('Saved timelapse to folder:',repr(folder))
+    
+save_run('-'.join([prompt_a,prompt_b])) #You can give it a good custom name if you want!
